d5f6223c297c2cb4ca44cdcabb39e40f60cc4c4b
Iw <iwor@list.ru>
2021-08-19 16:09:03 +0300

    edited CarController

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,27 +1,39 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
+    
+    @Autowired
+    private CarService carService;
 
     @GetMapping(value = "/cars")
-    public String printCars(ModelMap model) {
-        List<String> messages = new ArrayList<>();
-        messages.add((new Car("Nissan", "Qashqai", 1_500_000)).toString());
-        messages.add((new Car("Toyota", "RAV4", 2_000_000)).toString());
-        messages.add((new Car("Subaru", "Forester", 2_300_000)).toString());
-        messages.add((new Car("Mazda", "CX5", 2_200_000)).toString());
-        messages.add((new Car("Huyndai", "SantaFe", 2_500_000)).toString());
+    public String printCars(
+            ModelMap model,
+            @RequestParam(value = "count", required = false) Integer count
+    ) {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("Nissan", "Qashqai", 1_500_000));
+        carList.add(new Car("Toyota", "RAV4", 2_000_000));
+        carList.add(new Car("Subaru", "Forester", 2_400_000));
+        carList.add(new Car("Mazda", "CX5", 2_200_000));
+        carList.add(new Car("Huyndai", "SantaFe", 2_500_000));
         
-        model.addAttribute("messages", messages);
-        return "index";
+        List<String> list = new ArrayList<>();
+        carService.getCars(carList, count).forEach(car -> list.add(car.toString()));
+        
+        model.addAttribute("cars", list);
+        return "cars";
     }
 }
